e9161fa1a565b5f585cf05026d721a3d4eb0dade
Steve Smith <github@scsworld.co.uk>
2013-03-01 09:45:51 +0000

    Fix whitespace issues.

--- lib/language_list.rb
+++ lib/language_list.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module LanguageList
   class LanguageInfo
     attr_reader :name, :iso_639_3, :iso_639_1, :type
-    
+
     def initialize(options)
       @name = options[:name]
       @iso_639_3 = options[:iso_639_3]
@@ -11,37 +11,37 @@ module LanguageList
       @common = options[:common]
       @type = options[:type]
     end
-    
+
     def common?
       @common
     end
-    
+
     def <=>(other)
       self.name <=> other.name
     end
-    
+
     def iso_639_1?
       !@iso_639_1.nil?
     end
-    
+
     [:ancient, :constructed, :extinct, :historical, :living, :special].each do |type|
       define_method("#{type.to_s}?") do
         @type == type
       end
     end
-    
+
     def to_s
       "#{@iso_639_3}#{" (#{@iso_639_1})" if @iso_639_1} - #{@name}"
     end
-    
+
     def self.find_by_iso_639_1(code)
       LanguageList::ISO_639_1.detect{|l| l.iso_639_1 == code }
     end
-    
+
     def self.find_by_iso_639_3(code)
       LanguageList::ALL_LANGUAGES.detect{|l| l.iso_639_3 == code }
     end
-    
+
     def self.find_by_name(name)
       LanguageList::ALL_LANGUAGES.detect{|l| l.name == name }
     end
